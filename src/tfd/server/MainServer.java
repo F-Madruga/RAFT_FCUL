c15abd725d65cb3b34d66e42688f3db376542c43
Francisco Madruga <madruga@pop-os.localdomain>
2020-10-07 20:06:38 +0100

    Change of Server to ServerSocket

--- src/tfd/server/MainServer.java
+++ src/tfd/server/MainServer.java
@@ -1,49 +1,14 @@
 package tfd.server;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.util.Arrays;
 
 import tfd.configuration.Configuration;
 
 public class MainServer {
 
-	public static void getNetworkIPs() {
-		final byte[] ip;
-		try {
-			ip = InetAddress.getLocalHost().getAddress();
-		} catch (Exception e) {
-			return;     // exit method, otherwise "ip might not have been initialized"
-		}
-
-		for(int i=1;i<=254;i++) {
-			final int j = i;  // i as non-final variable cannot be referenced from inner class
-			new Thread(new Runnable() {   // new thread for parallel execution
-				public void run() {
-					try {
-						ip[3] = (byte)j;
-						InetAddress address = InetAddress.getByAddress(ip);
-						String output = address.toString().substring(1);
-						if (address.isReachable(5000)) {
-							System.out.println(output + " is on the network");
-						} else {
-							System.out.println("Not Reachable: "+output);
-						}
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}).start();     // dont forget to start the thread
-		}
-	}
-
 	public static void main(String[] args) throws IOException {
 		Configuration.load();
-		// find all servers
-		getNetworkIPs();
-		new Server(Integer.parseInt(args[0]), new ClientConnectionHandler());
-		//new Server(Configuration.getInt("PORT", 8080), new ClientConnectionHandler());
-		//new Server(Configuration.getInt("SERVER_PORT", 8081), new ServerConnectionHandler());
+		new ServerSocket(Configuration.getInt("CLIENT_PORT", 8080), new ClientConnectionHandler());
+		new ServerSocket(Configuration.getInt("SERVER_PORT", 8081), new ServerConnectionHandler());
 	}
-
 }
--- src/tfd/server/ServerSocket.java
+++ src/tfd/server/ServerSocket.java
@@ -3,12 +3,12 @@ package tfd.server;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 
-public class Server {
-	public Server(int port, IStreamHandler streamHandler) {
+public class ServerSocket {
+	public ServerSocket(int port, IStreamHandler streamHandler) {
 	    this(port, 100, streamHandler);
 	}
 	
-	public Server(int port, int threadPoolSize, IStreamHandler streamHandler) {
+	public ServerSocket(int port, int threadPoolSize, IStreamHandler streamHandler) {
 	    ExecutorService clientPool = Executors.newFixedThreadPool(threadPoolSize);
 	    Thread serverThread = new Thread(new ServerThread(clientPool, port, streamHandler));
 	    serverThread.start();
