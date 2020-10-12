6abb9fa6cbefe71d7e1a02d42b649d364e7bb8d9
João Loureiro <fc46796@alunos.fc.ul.pt>
2020-10-12 11:21:31 +0100

    FIX: single permanent connection per client

--- src/tfd/rpc/EmptyResponse.java
+++ src/tfd/rpc/EmptyResponse.java
@@ -5,7 +5,7 @@ public class EmptyResponse extends RPCMessage {
 	private static final long serialVersionUID = 1L;
 
 	public EmptyResponse() {
-		super(RPCMethod.NO_VOTE_RESPONSE, "");
+		super(RPCMethod.EMPTY_RESPONSE, "");
 	}
 
 }
