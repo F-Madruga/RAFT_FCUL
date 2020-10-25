a3cb0e89509cd88bd321b2420c0d3d4213bf6baf
Francisco Madruga <madruga@pop-os.localdomain>
2020-10-25 18:15:37 +0000

    Switch to node js

--- src/tfd/server/IMessageHandler.java
+++
@@ -1,9 +0,0 @@
-package tfd.server;
-
-import tfd.rpc.RPCMessage;
-
-public interface IMessageHandler {
-
-	public RPCMessage handle(RPCMessage message, String clientId);
-
-}
--- src/tfd/server/RaftState.java
+++
@@ -1,7 +0,0 @@
-package tfd.server;
-
-public enum RaftState {
-	LEADER,
-	FOLLOWER,
-	CANDIDATE,
-}
